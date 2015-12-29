308b7e3cbf3647ba97ce448dd28aef7a443151d1
Vitalii Onyshchenko <uzer2008@gmail.com>
2015-12-29 15:37:53 +0200

    Add DBAL

--- web/index.php
+++ web/index.php
@@ -1,13 +1,12 @@
 <?php
-$loader = require_once __DIR__.'/../vendor/autoload.php';
-$loader->addPsr4('Jtest\\', array(__DIR__.'/../app/controllers'));
-
 use Symfony\Component\HttpFoundation\Request;
 Request::enableHttpMethodParameterOverride();
 
+$loader = require_once __DIR__.'/../vendor/autoload.php';
+$loader->addPsr4('Jtest\\', array(__DIR__.'/../app/controllers'));
+
 $app = new Silex\Application();
 $app['debug'] = true;
-
 
 $app->mount('/contact', new Jtest\ContactController());
 
